34bca8697685df98b28364676ea30272f797667a
sksaini9796 <shorabhkumarsaini@gmail.com>
2024-06-14 11:31:28 +0000

    .

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -11,7 +11,7 @@ function App() {
     <>
       <h1 className='bg-green-400 text-black p-4
       rounded-xl' >Tailwind</h1>
-      <Card />
+      <Card username="chaiaurcode" btnText="click me" />
       <Card />
     </>
   )
--- 03tailwindprops/src/Card.jsx
+++ 03tailwindprops/src/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Card() {
+function Card({username, btnText}) {
   return (
     <div class="relative h-[400px] w-[300px] rounded-md">
   <img
@@ -10,13 +10,13 @@ function Card() {
   />
   <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
   <div class="absolute bottom-4 left-4 text-left">
-    <h1 class="text-lg font-semibold text-white">Delba</h1>
+    <h1 class="text-lg font-semibold text-white">{username}</h1>
     <p class="mt-2 text-sm text-gray-300">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
       debitis?
     </p>
-    <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-      View Profile →
+    <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-black">
+      {btnText || "visit me"}
     </button>
   </div>
 </div>
